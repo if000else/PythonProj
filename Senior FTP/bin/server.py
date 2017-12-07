6c287f8f1b00345854d0e83eb629fcc7a9f96072
huayiqiu <284527552@qq.com>
2017-12-07 14:32:46 +0800

    first commit in pycharm。。。

--- Senior FTP/bin/server.py
+++ Senior FTP/bin/server.py
@@ -4,57 +4,121 @@ BASEDIR = Path(__file__).parent.parent
 sys.path.append(str(BASEDIR))
 from conf import settings
 from modules import log
-
+Bash = {
+    'login':'self.login()',
+    'logout':'self.logout()',
+    'cd':'self.cd()',
+    'rz':'self.rz()',
+    'sz':'self.sz()',
+    'useradd':'self.useradd()',
+    'ls':'self.ls()',
+}
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
-    LoginUsers = []  ### logined users
-    PATH=settings.HomeOfServer### home dir
+    User=None
+    PATH=settings.HomeOfServer  ### home dir
+    Command = []
     def handle(self):
         print("client [%s] in.." % self.client_address[0])
         while True:
             comm = self.request.recv(1024)
-            if comm.decode() == 'login':
-                self.request.send(b'Please input username:')
-                user=self.request.recv(1024).decode()
-                if user =='quit':
-                    self.request.send(b'quit')
-                self.request.send(b'Please input password:')
-                psd=self.request.recv(1024).decode()
-                with open('%s/date.db'%settings.USER_DATA,'r') as f:
-                    date = json.load(f)
-                    if date.get(user):
-                        if psd == date[user]:
-                            print("client [%s] has login..."%self.client_address[0])
-                            self.request.send(b'Welcome!')
-                            self.LoginUsers.append(user)
-                            self.operate()
-                        else:
-                            self.request.send(b'Incorrect user or password!')
-                    else:
-                        self.request.send(b'No such user!')
+            self.Command = comm.strip().split()
+            for func in Bash:
+                if self.Command[0]==func:
+                    eval(Bash[func])  # call corresponding functions
 
-    def operate(self):
-        try:
-            while True:
-                # self.request.send(b'welcome!')
-                data=self.request.recv(1024).decode()
-                if data == 'ls':
-                    pass
-                elif data == 'rz':
-                    pass
-                elif data == 'sz':
-                    pass
-                elif data == 'cd':
-                    pass
-                elif data == 'useradd':
-                    pass
-                elif data == 'logout':
-                    pass
+
+    def ls(self):
+        if self.User:
+            pass
+        else:
+            self.login()
+    def cd(self):
+        pass
+    def rz(self):
+        pass
+    def sz(self):
+        pass
+    def useradd(self):
+        pass
+    def login(self):
+        '''
+        interact with user while login,if success,set User
+        :return:
+        '''
+        while True:
+            self.request.send(b'Please input username:')
+            user = self.request.recv(1024).decode()
+            if user == 'quit':
+                self.request.send(b'quit')
+                break
+            self.request.send(b'Please input password:')
+            psd = self.request.recv(1024).decode()
+            with open('%s/date.db' % settings.USER_DATA, 'r') as f:
+                date = json.load(f)
+                if date.get(user):
+                    if psd == date[user]:
+                        print("client [%s] has login..." % self.client_address[0])
+                        self.request.send(b'success to login')
+                        self.User=user
+                    else:
+                        self.request.send(b'Incorrect username or password!') # failed to login
                 else:
-                    self.request.send(b'')
-        except Exception :
-            print("Exist error!!!")
+                    self.request.send(b'Incorrect username or password!')  # failed to login
+    def logout(self):
+        '''
+        request of logout by client,set None User.
+        :return:
+        '''
+        self.User=None
+        self.request.send(b'logout...')
+
+
+
+
+if comm == 'login':
+    self.request.send(b'Please input username:')
+    user=self.request.recv(1024).decode()
+    if user =='quit':
+        self.request.send(b'quit')
+        break
+    self.request.send(b'Please input password:')
+    psd=self.request.recv(1024).decode()
+    with open('%s/date.db'%settings.USER_DATA,'r') as f:
+        date = json.load(f)
+        if date.get(user):
+            if psd == date[user]:
+                print("client [%s] has login..."%self.client_address[0])
+                self.request.send(1)
+                self.LoginUsers.append(user)
+                self.operate()
+            else:
+                self.request.send(0)
+        else:
+            self.request.send(0)# failed to login
+
+# def operate(self):
+#     try:
+#         while True:
+#             # self.request.send(b'welcome!')
+#             data=self.request.recv(1024).decode()
+#             if data == 'ls':
+#                 pass
+#             elif data == 'rz':
+#                 pass
+#             elif data == 'sz':
+#                 pass
+#             elif data == 'cd':
+#                 pass
+#             elif data == 'useradd':
+#                 pass
+#             elif data == 'logout':
+#                 pass
+#             else:
+#                 self.request.send(b'')
+#     except Exception :
+#         print("Exist error!!!")
 
 if __name__ == "__main__":
     server=socketserver.ThreadingTCPServer((settings.PORT,settings.ADDR),MyTCPHandler)
